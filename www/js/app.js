9020f89a848da1501c33a152b3674606adf2f434
Ino Van Winckel <inovanwinckel@hotmail.com>
2017-01-08 12:25:32 +0100

    Ionic cloud added, push notifications

--- www/js/app.js
+++ www/js/app.js
@@ -5,83 +5,98 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services', 'app.constants', 'ngCordovaOauth', 'ngCordova', 'spotify'])
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives', 'app.services', 'app.constants', 'ngCordovaOauth', 'ngCordova', 'spotify', 'ionic.cloud'])
 
-.config(function($ionicConfigProvider, $sceDelegateProvider){
+  .config(function ($ionicCloudProvider, $ionicConfigProvider, $sceDelegateProvider) {
+    $ionicCloudProvider.init({
+      "core": {
+        "app_id": "334f2990"
+      },
+      "push": {
+        "sender_id": "991771295875",
+        "pluginConfig": {
+          "android": {
+            "iconColor": "#343434"
+          }
+        }
+      }
+    });
 
     $ionicConfigProvider.tabs.position('bottom');
-    $sceDelegateProvider.resourceUrlWhitelist([ 'self','*://www.youtube.com/**', '*://player.vimeo.com/video/**']);
+    $sceDelegateProvider.resourceUrlWhitelist(['self', '*://www.youtube.com/**', '*://player.vimeo.com/video/**']);
 
-})
+  })
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.run(function ($rootScope, $state, LoginService) {
-    $rootScope.$on('$stateChangeStart', function (event,next, nextParams, fromState) {
-        LoginService.isLoggedIn();
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
     });
-})
+  })
 
-/*
-  This directive is used to disable the "drag to open" functionality of the Side-Menu
-  when you are dragging a Slider component.
-*/
-.directive('disableSideMenuDrag', ['$ionicSideMenuDelegate', '$rootScope', function($ionicSideMenuDelegate, $rootScope) {
+  .run(function ($rootScope, $state, LoginService) {
+    $rootScope.$on('$stateChangeStart', function (event, next, nextParams, fromState) {
+      LoginService.isLoggedIn();
+    });
+
+
+  })
+
+  /*
+   This directive is used to disable the "drag to open" functionality of the Side-Menu
+   when you are dragging a Slider component.
+   */
+  .directive('disableSideMenuDrag', ['$ionicSideMenuDelegate', '$rootScope', function ($ionicSideMenuDelegate, $rootScope) {
     return {
-        restrict: "A",
-        controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
+      restrict: "A",
+      controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
 
-            function stopDrag(){
-              $ionicSideMenuDelegate.canDragContent(false);
-            }
+        function stopDrag() {
+          $ionicSideMenuDelegate.canDragContent(false);
+        }
 
-            function allowDrag(){
-              $ionicSideMenuDelegate.canDragContent(true);
-            }
+        function allowDrag() {
+          $ionicSideMenuDelegate.canDragContent(true);
+        }
 
-            $rootScope.$on('$ionicSlides.slideChangeEnd', allowDrag);
-            $element.on('touchstart', stopDrag);
-            $element.on('touchend', allowDrag);
-            $element.on('mousedown', stopDrag);
-            $element.on('mouseup', allowDrag);
+        $rootScope.$on('$ionicSlides.slideChangeEnd', allowDrag);
+        $element.on('touchstart', stopDrag);
+        $element.on('touchend', allowDrag);
+        $element.on('mousedown', stopDrag);
+        $element.on('mouseup', allowDrag);
 
-        }]
+      }]
     };
-}])
+  }])
 
-/*
-  This directive is used to open regular and dynamic href links inside of inappbrowser.
-*/
-.directive('hrefInappbrowser', function() {
-  return {
-    restrict: 'A',
-    replace: false,
-    transclude: false,
-    link: function(scope, element, attrs) {
-      var place = attrs['hrefInappbrowser'] || '_system';
-      element.bind('click', function (event) {
+  /*
+   This directive is used to open regular and dynamic href links inside of inappbrowser.
+   */
+  .directive('hrefInappbrowser', function () {
+    return {
+      restrict: 'A',
+      replace: false,
+      transclude: false,
+      link: function (scope, element, attrs) {
+        var place = attrs['hrefInappbrowser'] || '_system';
+        element.bind('click', function (event) {
 
-        var href = event.currentTarget.href;
+          var href = event.currentTarget.href;
 
-        window.open(href, place, 'location=yes');
+          window.open(href, place, 'location=yes');
 
-        event.preventDefault();
-        event.stopPropagation();
+          event.preventDefault();
+          event.stopPropagation();
 
-      });
-    }
-  };
-});
+        });
+      }
+    };
+  });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,218 +1,232 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['ionic.cloud'])
 
-    .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  .controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-        function ($scope, $stateParams, $state) {
-            var menu = this;
+    function ($scope, $stateParams, $state) {
+      var menu = this;
 
-        }])
+    }])
 
-    .controller('settingsCtrl', function (SpotifyService) {
-        var settings = this;
+  .controller('settingsCtrl', function (SpotifyService) {
+    var settings = this;
 
-        settings.spotify = function () {
-            SpotifyService.login().then(function (res) {
-                console.log(res);
-            }, function (err) {
-                console.log(err);
-            });
-        }
-    })
+    settings.spotify = function () {
+      SpotifyService.login().then(function (res) {
+        console.log(res);
+      }, function (err) {
+        console.log(err);
+      });
+    }
+  })
 
-    .controller('newsfeedCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  .controller('newsfeedCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-        function ($scope, $stateParams) {
+    function ($scope, $stateParams, $ionicPush) {
+      $scope.$on('cloud:push:notification', function(event, data) {
+        var msg = data.message;
+        alert(msg.text);
+      });
 
+    }])
 
-        }])
-
-    .controller('myMusicCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  .controller('myMusicCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-        function ($scope, $stateParams, $http) {
-            $scope.getRecords = function () {
-                $http.get('http://188.226.129.26/records/get').then(function (records) {
-                    return records
-                });
-            }
-        }])
-
-    .controller('followingCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-        function ($scope, $stateParams) {
-
-
-        }])
-
-    .controller('profileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-        function ($scope, $stateParams) {
-
-
-        }])
-
-    .controller('recordCtrl', ['$scope', '$stateParams', '$http',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-        function ($scope, $stateParams, $http) {
-
-
-        }])
-
-    .controller('loginCtrl', function ($scope, $state, $q, UserService, $ionicLoading, $http) {
-        // This is the success callback from the login method
-        var fbLoginSuccess = function (response) {
-            if (!response.authResponse) {
-                fbLoginError("Cannot find the authResponse");
-                return;
-            }
-
-            var authResponse = response.authResponse;
-
-            getFacebookProfileInfo(authResponse)
-                .then(function (profileInfo) {
-                    // For the purpose of this example I will store user data on local storage
-                    UserService.setUser({
-                        authResponse: authResponse,
-                        userID: profileInfo.id,
-                        name: profileInfo.name,
-                        email: profileInfo.email,
-                        picture: "http://graph.facebook.com/" + authResponse.userID + "/picture?type=large"
-                    });
-
-                    var user = UserService.getUser();
-
-                    // Store user in DB
-                    $http.post('http://188.226.129.26/api/register', {
-                        firstname: profileInfo.first_name,
-                        lastname: profileInfo.last_name,
-                        email: profileInfo.email,
-                        token: user.facebook.authResponse.accessToken,
-                        api_user_id: user.facebook.userID
-                    }).then(function (response) {
-                        console.log(response);
-                        console.log('login token', accessToken);
-                    });
-
-                    /*facebookConnectPlugin.showDialog({
-                     method: "feed",
-                     link: 'https://spotify.com',
-                     message:'Test post from App',
-                     },
-                     function (response) { alert(JSON.stringify(response)) },
-                     function (response) { alert(JSON.stringify(response)) }
-                     );*/
-
-                    $ionicLoading.hide();
-                    $state.go('tabsController.newsfeed');
-                }, function (fail) {
-                    // Fail get profile info
-                    console.log('profile info fail', fail);
-                });
-        };
-
-        var token = '';
-
-        // This is the fail callback from the login method
-        var fbLoginError = function (error) {
-            console.log('fbLoginError', error);
-            $ionicLoading.hide();
-        };
-
-        // This method is to get the user profile info from the facebook api
-        var getFacebookProfileInfo = function (authResponse) {
-            var info = $q.defer();
-            accessToken = authResponse.accessToken
-
-            facebookConnectPlugin.api('/me?fields=email,first_name,last_name&access_token=' + accessToken, null,
-                function (response) {
-                    console.log(response);
-                    info.resolve(response);
-                },
-                function (response) {
-                    console.log(response);
-                    info.reject(response);
-                }
-            );
-            return info.promise;
-        };
-
-        //This method is executed when the user press the "Login with facebook" button
-        $scope.facebookSignIn = function () {
-            facebookConnectPlugin.getLoginStatus(function (success) {
-                if (success.status === 'connected') {
-                    // The user is logged in and has authenticated our app, and response.authResponse supplies
-                    // the user's ID, a valid access token, a signed request, and the time the access token
-                    // and signed request each expire
-
-                    // Check if we have our user saved
-                    var user = UserService.getUser('facebook');
-                    console.log(user);
-
-                    if (!user.facebook.userID) {
-                        getFacebookProfileInfo(success.authResponse)
-                            .then(function (profileInfo) {
-
-                                // Store user data on local storage
-                                UserService.setUser({
-                                    authResponse: success.authResponse,
-                                    userID: profileInfo.id,
-                                    name: profileInfo.name,
-                                    email: profileInfo.email,
-                                    picture: "http://graph.facebook.com/" + success.authResponse.userID + "/picture?type=large"
-                                });
-
-                                var user = UserService.getUser();
-
-                                // Store user in DB
-                                $http.post('http://188.226.129.26/api/register', {
-                                    firstname: profileInfo.first_name,
-                                    lastname: profileInfo.last_name,
-                                    email: profileInfo.email,
-                                    token: user.facebook.authResponse.accessToken,
-                                    api_user_id: user.facebook.userID
-                                }).then(function (response) {
-                                    console.log(response.status);
-                                });
-
-                                $state.go('tabsController.newsfeed');
-                            }, function (fail) {
-                                // Fail get profile info
-                                console.log('profile info fail', fail);
-                            });
-                    } else {
-                        $state.go('tabsController.newsfeed');
-                    }
-                } else {
-                    // If (success.status === 'not_authorized') the user is logged in to Facebook,
-                    // but has not authenticated your app
-                    // Else the person is not logged into Facebook,
-                    // so we're not sure if they are logged into this app or not.
-
-                    console.log('getLoginStatus', success.status);
-
-                    $ionicLoading.show({
-                        template: 'Logging in...'
-                    });
-
-                    // Ask the permissions you need. You can learn more about
-                    // FB permissions here: https://developers.facebook.com/docs/facebook-login/permissions/v2.4
-                    facebookConnectPlugin.login(['email', 'public_profile'], fbLoginSuccess, fbLoginError);
-                }
-            });
-        };
-    })
-
-    .controller('logoutCtrl', function ($scope, UserService, $ionicActionSheet, $state) {
-        $scope.user = UserService.getUser();
-
-        facebookConnectPlugin.logout(function () {
-            console.log('Logging out...');
+    function ($scope, $stateParams, $http) {
+      $scope.getRecords = function () {
+        $http.get('http://188.226.129.26/records/get').then(function (records) {
+          return records
         });
-        $state.go('login');
-        console.log('boom!');
-    })
+      }
+    }])
+
+  .controller('followingCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {
+
+
+    }])
+
+  .controller('profileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams) {
+
+
+    }])
+
+  .controller('recordCtrl', ['$scope', '$stateParams', '$http',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+    function ($scope, $stateParams, $http) {
+
+
+    }])
+
+  .controller('loginCtrl', function ($scope, $state, $q, UserService, $ionicLoading, $http, $ionicPush) {
+    // This is the success callback from the login method
+    var fbLoginSuccess = function (response) {
+      $ionicLoading.hide();
+
+      if (!response.authResponse) {
+        fbLoginError("Cannot find the authResponse");
+        return;
+      }
+
+      $ionicPush.register().then(function (t) {
+        return $ionicPush.saveToken(t);
+      }).then(function (t) {
+        console.log('Push token saved: ' + t.token)
+      });
+
+
+      var authResponse = response.authResponse;
+
+      getFacebookProfileInfo(authResponse)
+        .then(function (profileInfo) {
+          // For the purpose of this example I will store user data on local storage
+          UserService.setUser({
+            authResponse: authResponse,
+            userID: profileInfo.id,
+            name: profileInfo.name,
+            email: profileInfo.email,
+            picture: "http://graph.facebook.com/" + authResponse.userID + "/picture?type=large"
+          });
+
+          var user = UserService.getUser();
+
+          // Store user in DB
+          $http.post('http://188.226.129.26/api/register', {
+            firstname: profileInfo.first_name,
+            lastname: profileInfo.last_name,
+            email: profileInfo.email,
+            token: user.facebook.authResponse.accessToken,
+            api_user_id: user.facebook.userID
+          }).then(function (response) {
+            console.log(response);
+            console.log('login token', accessToken);
+          });
+
+          /*facebookConnectPlugin.showDialog({
+           method: "feed",
+           link: 'https://spotify.com',
+           message:'Test post from App',
+           },
+           function (response) { alert(JSON.stringify(response)) },
+           function (response) { alert(JSON.stringify(response)) }
+           );*/
+
+          $ionicLoading.hide();
+          $state.go('tabsController.newsfeed');
+        }, function (fail) {
+          // Fail get profile info
+          console.log('profile info fail', fail);
+        });
+    };
+
+    var token = '';
+
+    // This is the fail callback from the login method
+    var fbLoginError = function (error) {
+      console.log('fbLoginError', error);
+      $ionicLoading.hide();
+    };
+
+    // This method is to get the user profile info from the facebook api
+    var getFacebookProfileInfo = function (authResponse) {
+      var info = $q.defer();
+      accessToken = authResponse.accessToken
+
+      facebookConnectPlugin.api('/me?fields=email,first_name,last_name&access_token=' + accessToken, null,
+        function (response) {
+          console.log(response);
+          info.resolve(response);
+        },
+        function (response) {
+          console.log(response);
+          info.reject(response);
+        }
+      );
+      return info.promise;
+    };
+
+    //This method is executed when the user press the "Login with facebook" button
+    $scope.facebookSignIn = function () {
+      facebookConnectPlugin.getLoginStatus(function (success) {
+        if (success.status === 'connected') {
+          // The user is logged in and has authenticated our app, and response.authResponse supplies
+          // the user's ID, a valid access token, a signed request, and the time the access token
+          // and signed request each expire
+
+          // Check if we have our user saved
+          var user = UserService.getUser('facebook');
+          console.log(user);
+
+          if (!user.facebook.userID) {
+            getFacebookProfileInfo(success.authResponse)
+              .then(function (profileInfo) {
+
+                // Store user data on local storage
+                UserService.setUser({
+                  authResponse: success.authResponse,
+                  userID: profileInfo.id,
+                  name: profileInfo.name,
+                  email: profileInfo.email,
+                  picture: "http://graph.facebook.com/" + success.authResponse.userID + "/picture?type=large"
+                });
+
+                var user = UserService.getUser();
+
+                // Store user in DB
+                $http.post('http://188.226.129.26/api/register', {
+                  firstname: profileInfo.first_name,
+                  lastname: profileInfo.last_name,
+                  email: profileInfo.email,
+                  token: user.facebook.authResponse.accessToken,
+                  api_user_id: user.facebook.userID
+                }).then(function (response) {
+                  console.log(response.status);
+                });
+
+                $state.go('tabsController.newsfeed');
+              }, function (fail) {
+                // Fail get profile info
+                console.log('profile info fail', fail);
+              });
+          } else {
+            $state.go('tabsController.newsfeed');
+          }
+        } else {
+          // If (success.status === 'not_authorized') the user is logged in to Facebook,
+          // but has not authenticated your app
+          // Else the person is not logged into Facebook,
+          // so we're not sure if they are logged into this app or not.
+
+          console.log('getLoginStatus', success.status);
+
+          $ionicLoading.show({
+            template: 'Logging in...'
+          }).then(function () {
+            facebookConnectPlugin.login(['email', 'public_profile'], fbLoginSuccess, fbLoginError);
+          });
+
+          // Ask the permissions you need. You can learn more about
+          // FB permissions here: https://developers.facebook.com/docs/facebook-login/permissions/v2.4
+
+        }
+      });
+    };
+  })
+
+  .controller('logoutCtrl', function ($scope, UserService, $ionicActionSheet, $state) {
+    $scope.user = UserService.getUser();
+
+    facebookConnectPlugin.logout(function () {
+      console.log('Logging out...');
+    });
+    $state.go('login');
+    console.log('boom!');
+  })
